448bc7c33a470bcd06b23950eb26272b6b9333d1
Arun Kumar <pheron408@gmail.com>
2026-08-05 16:52:07 +0530

    Intialize the spring boot app

---
+++ src/main/java/com/arun/backend/controller/DemoController.java
@@ -0,0 +1,12 @@
+package com.arun.backend.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello world";
+    }
+}
