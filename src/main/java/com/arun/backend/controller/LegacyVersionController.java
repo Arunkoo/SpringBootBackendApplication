f6c67e851856ed1a7d0de416c1f9e525294c1e8b
Arun Kumar <pheron408@gmail.com>
2026-08-18 17:21:12 +0530

    Implemented Api versioning using reqParams

--- src/main/java/com/arun/backend/controller/LegacyVersionController.java
+++ src/main/java/com/arun/backend/controller/LegacyVersionController.java
@@ -3,11 +3,14 @@ package com.arun.backend.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/legacy/version")
 public class LegacyVersionController {
+
+    //using a path versioning...
     @GetMapping({"", "/", "/v1"})
     public ResponseEntity<String> defaultPathVersion(){
         return ResponseEntity.ok("Response from default path version API 1.0.0");
@@ -16,5 +19,16 @@ public class LegacyVersionController {
     @GetMapping("/v2")
     public ResponseEntity<String> pathV2Version(){
         return ResponseEntity.ok("Response from pathV2Version API 2.0.0");
+    }
+
+    //using a reqParams versioning...
+    @GetMapping(params = "version=1")
+    public ResponseEntity<String> defaultReqParamsVersion(){
+        return ResponseEntity.ok("Response from default reqParams version API 1.0.0");
+    }
+
+    @GetMapping(params = "version=2")
+    public ResponseEntity<String> reqParamsV2Version(){
+        return ResponseEntity.ok("Response from reqParamsV2Version API 2.0.0");
     }
 }
